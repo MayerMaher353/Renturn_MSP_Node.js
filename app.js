85acc0fa23ebb19d05fc37960bcaf7facb308bef
seif <seifmedhat120@gmail.com>
2025-08-19 02:45:27 +0300

    make productControllers ,postProduct validation and errorHandler middleware. modified on product model and category model .also put all routes in app.js

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const connDb = require("./config/Data_Connection");
 const categoryRoute = require("./Routes/categoryRoute");
+const errorHandler = require("./middlewares/errorHandler");
+const productRoute =require("./Routes/productRoute")
 
 const app = express();
+//Error handler middleware
+app.use(errorHandler);
 
 // middleware body
 app.use(express.json());
@@ -14,7 +18,11 @@ connDb();
 // mount routes
 app.use("/api/v1/categories", categoryRoute);
 
-
+/**
+ *  @description CRUD products
+ *  @route /api/v1/products or /api/v1/products/:id
+ */
+app.use("/api/v1/products",productRoute)
 // listening to server
 app.listen(process.env.PORT || 5000, () => {
   console.log("running");
