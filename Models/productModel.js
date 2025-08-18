4ac9aa724ad0e352abfcc392a358b388fefebab8
seif <seifmedhat120@gmail.com>
2025-08-18 21:21:57 +0300

    create product model and cart model

--- Models/productModel.js
+++ Models/productModel.js
@@ -1,10 +1,28 @@
-const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 
 const productModel = new mongoose.Schema({
     product_name:{
         type:String,
-        required:true ,
-        
-    }
+        required:true,
+        trim :true,
+    },
+    product_description:{
+        type:String,
+        required:false,
+        trim:true,
+        maxlength:100,
+    },
+    qunatity:{
+        type:Number,
+        min:0,
+    },
+    price:{
+        type:Number,
+        min:0,
+    },
+}) ;
 
-}) 
+
+const product = mongoose.model('product',productModel)
+
+module.exports= product ;
