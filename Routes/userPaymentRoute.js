1de42600794b07f8468b1cd5cae4858360550419
seif <seifmedhat120@gmail.com>
2025-09-02 04:53:58 +0300

    don't use this update branch

--- Routes/userPaymentRoute.js
+++ Routes/userPaymentRoute.js
@@ -4,8 +4,8 @@ const {getAvailableGateWay,chooseGateWay,createCheckout}= require("../Controller
 const { protect } = require("../middlewares/authMiddleware");
 
 
-route.get("/availablePayment",protect,getAvailableGateWay)
-route.post("/chooseGateWay",protect,chooseGateWay)
-route.post("/checkout", protect,createCheckout);
+route.get("/availablePayment",protect,getAvailableGateWay) //https://renturn.vercel.app/userPayment/availablePayment or http://localhost:5000/api/v1/userPayment/availablePayment
+route.post("/choose-gateway",protect,chooseGateWay);//https://renturn.vercel.app/userPayment/chooseGateWay or http://localhost:5000/api/v1/userPayment/choose-gateway
+route.post("/checkout", protect,createCheckout);//https://renturn.vercel.app/userPayment/checkout or http://localhost:5000/api/v1/userPayment/checkout
 
 module.exports=route;
